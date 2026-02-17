d652c5ac79d94012e3009d8f6086a858219d8911
sadhu.sreenivas <sadhu.sreenivas@gmail.com>
2026-02-17 22:49:02 +0800

    changed the api methods to get,put,post and delete

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,4 +1,4 @@
-import { authOptions } from '@/app/api/auth/[...nextauth]/route'
+import { authOptions } from '@/lib/auth'
 import EmployerDashboard from '@/components/EmployerDashboard'
 import JobSeekerDashboard from '@/components/JobSeekerDashboard'
 import { getServerSession } from 'next-auth'
